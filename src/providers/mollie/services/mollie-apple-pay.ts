f3cff6de876ec2bcbf1dde50d2076adea4bba732
Victor Gerbrands <victorgerbrands@gmail.com>
2025-03-10 13:07:45 +0100

    chore: rm logs

--- src/providers/mollie/services/mollie-apple-pay.ts
+++ src/providers/mollie/services/mollie-apple-pay.ts
@@ -6,21 +6,15 @@ class MollieApplePayService extends MollieBase {
   static identifier = PaymentProviderKeys.APPLE_PAY;
 
   get paymentCreateOptions(): PaymentOptions {
-    console.log("vic logs apple pay");
-    try {
-      return {
-        method: PaymentMethod.applepay,
-        webhookUrl:
-          this.options_.medusaUrl +
-          "/hooks/payment/" +
-          PaymentProviderKeys.APPLE_PAY +
-          "_mollie",
-        captureMethod: CaptureMethod.automatic,
-      };
-    } catch (error) {
-      console.log("vic logs apple pay error", error);
-      throw error;
-    }
+    return {
+      method: PaymentMethod.applepay,
+      webhookUrl:
+        this.options_.medusaUrl +
+        "/hooks/payment/" +
+        PaymentProviderKeys.APPLE_PAY +
+        "_mollie",
+      captureMethod: CaptureMethod.automatic,
+    };
   }
 }
 
